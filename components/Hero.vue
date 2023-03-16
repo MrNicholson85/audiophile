bfe9a766934eb503b9829992107253ce17390403
Darryl <darryl.a.nicholson@gmail.com>
2023-03-15 21:14:09 -0700

    hero component polish pass
    updated hero components with responsive images and styled breakpoints

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,26 +1,28 @@
 <template>
   <div v-for="p in data">
     <div v-if="p.id === 4">
-      <div class="hero">
+      <div class="hero h-[600px] lg:h-[769px]">
         <div
-          class="container mx-auto flex h-[768px] justify-between gap-x-11 bg-cover bg-center bg-no-repeat sm:bg-[url('/assets/images/image-header-mobile.jpg')] lg:bg-[url('/assets/images/HomePageHero.png')]"
+          class="md:container mx-auto flex h-[600px] lg:h-[768px] justify-between gap-x-11 bg-[0_-110px] bg-cover lg:bg-center bg-no-repeat bg-[url('/assets/images/image-header-mobile.jpg')] lg:bg-[url('/assets/images/HomePageHero.png')] md:bg-[url('/assets/images/image-header-tablet.jpg')]"
         >
           <div
-            class="text-white lg:w-1/2 sm:w-full grid grid-cols-1 place-content-center lg:text-left sm:text-center"
+            class="text-white lg:w-1/2 sm:w-full grid grid-cols-1 place-content-center lg:text-left text-center"
           >
-            <p class="text-sm tracking-[10px] opacity-[49.64%]">NEW PRODUCT</p>
+            <p class="text-sm tracking-[10px] opacity-[49.64%] mb-6">
+              NEW PRODUCT
+            </p>
             <p class="text-[56px] leading-[58px] mb-6 uppercase font-bold my-1">
               {{ p.name }}
             </p>
             <p
-              class="text-[15px] w-[349px] sm:mx-auto lg:m-0 opacity-[75%] leading-[25px]"
+              class="text-[15px] w-[328px] lg:w-[349px] mx-auto lg:m-0 opacity-[75%] leading-[25px] px-2 lg:mb-10"
             >
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
             <NuxtLink
               :to="`/products/3`"
-              class="btn-primary hover:btn-primary-lighten w-[160px] sm:mx-auto lg:m-0 text-center"
+              class="btn-primary hover:btn-primary-lighten w-[160px] mx-auto lg:m-0 text-center"
             >
               SEE PRODUCT
             </NuxtLink>
@@ -32,8 +34,6 @@
 </template>
 
 <script setup>
-import heroImage from "@/assets/images/HomePageHero.png";
-import heroImageMobile from "@/assets/images/image-header-mobile.png";
 import data from "../data/data.json";
 </script>
 
@@ -42,7 +42,6 @@ import data from "../data/data.json";
   top: 0;
   width: 100%;
   background-color: #141414;
-  height: 768px;
   z-index: 1;
 }
 </style>
